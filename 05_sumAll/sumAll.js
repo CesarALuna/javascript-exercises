fe224db532902e0d85f91a174157953980ed6e38
CesarALuna <ceslun@gmail.com>
2022-09-10 03:45:43 -0700

    Finish exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  // no non-numbers
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  // no negative numbers
+  if (a < 0 || b < 0) return "ERROR";
 
+  // if a is larger than b, we need them to trade places
+  if (a > b) {
+    const temp = a;
+    a = b;
+    b = temp;
+  }
+  // initialize sum
+  let sum = 0;
+  //    for loop. i starts at smaller value
+  //    while i is smaller than larger value +1
+  //    iterating 1 at a time
+  //    add the value of i to sum
+  for (let i = a; i < b + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
